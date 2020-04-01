68bc5502b0f7b6d80b4d82f183cd0fed17f90d85
mrueda020 <mrueda020@hotmail.com>
2020-04-01 17:52:33 -0600

    Added ObjectiveActor class

---
+++ Source/FPSGame/Public/FPSObjectiveActor.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "FPSObjectiveActor.generated.h"
+
+class USphereComponent;
+
+UCLASS()
+class FPSGAME_API AFPSObjectiveActor : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	AFPSObjectiveActor();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	// To add a representation of the actor in the world
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	UStaticMeshComponent* MeshComp;
+
+	// Sets the collision for the actor
+	UPROPERTY(VisibleAnywhere, Category = "Components")
+	USphereComponent* SphereComp;
+
+
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
